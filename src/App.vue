b3f2ff4e2758c3b72d4a8935c3e250c3c0e28e24
Noam Kleiner <noam.k@compie.co.il>
2024-04-11 03:00:37 +0300

    switch back to vue-simple-camera

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,24 @@
 <template>
   <div class="container">
     <Button :text primary @click="() => (cameraActive = true)" />
-    <Button :text="'TAKE PICTURE'" primary @click="takePicture" />
-    <video id="webcam" autoplay playsinline width="640" height="480"></video>
-    <canvas id="canvas" class="d-none"></canvas>
+
+    <template v-if="cameraActive">
+      <camera ref="camera" autoplay></camera>
+      <button @click="takePicture">Create snapshot</button>
+    </template>
   </div>
 </template>
 <script setup lang="js">
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 import Button from "./components/Button.vue";
-import Webcam from "webcam-easy";
 
 const text = "Click to scan item".toUpperCase();
-
 const cameraActive = ref(false);
-
-const webcam = ref();
-onMounted(() => {
-  const webcamElement = document.getElementById("webcam");
-  const canvasElement = document.getElementById("canvas");
-  webcam.value = new Webcam(webcamElement, "user", canvasElement);
-  webcam.value.start();
-});
 
 const takePicture = () => {
   const picture = webcam.value.snap();
   console.log(picture);
 };
-
-// const resolution =
 </script>
 
 <style scoped lang="scss">
